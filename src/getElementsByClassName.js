d73b071e6d1c5b1dfeb0e32f236b8d369226c468
drumgarage <drumgarage@gmail.com>
2022-05-31 14:54:12 -0700

    Completed getElementsByClassName();  Changed order of code to what my pair had initially suggested.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,43 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  var elements = []
+  // input is element
+  var innerFunc = function(node) {
+    // check for calssList at node
+    // check that classList at node has className
+    if (node.classList && node.classList.contains(className)) {
+      // if node at classList exists and node at classList contains classname / push to elements array
+      elements.push(node);
+    }
+    // check if node has children
+    if (node.childNodes) {
+      // iterate over children (forEach) calling innerFunc on each child node
+      node.childNodes.forEach(function(childNode) {
+        innerFunc(childNode);
+      })
+    }
+  };
+  innerFunc(document.body);
+  // return elements array;
+  return elements;
 };
+
+  // old code ///
+
+
+    // if (!node.childNodes) {
+    //   //check current node for presense of classname
+
+    //   if (classArray.contains(className)) {
+
+    //     elements.push(node)
+    //   }
+    // }
+    //grab childNodes
+    // var children = node.childNodes;
+
+
+    //call innerfunc on child node
+    //input as array
